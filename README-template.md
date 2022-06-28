d70cfa660673a2826c55bf644691250d1452b40e
ibimina <96787210+ibimina@users.noreply.github.com>
2022-06-28 10:37:13 +0100

    commit

---
+++ README-template.md
@@ -0,0 +1,112 @@
+# Frontend Mentor - Crowdfunding product page solution
+
+This is a solution to the [Crowdfunding product page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/crowdfunding-product-page-7uvcZe7ZR). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout depending on their device's screen size
+- See hover states for interactive elements
+- Make a selection of which pledge to make
+- See an updated progress bar and total money raised based on their pledge total after confirming a pledge
+- See the number of total backers increment by one after confirming a pledge
+- Toggle whether or not the product is bookmarked
+
+### Screenshot
+
+![mobile](Capture81.png)
+![desktop](Capture82.png)
+
+### Links
+
+- Solution URL: [https://github.com/ibimina/crowfunding-product-page-main](https://github.com/ibimina/crowfunding-product-page-main)
+- Live Site URL: [https://ibimina.github.io/crowfunding-product-page-main/](https://ibimina.github.io/crowfunding-product-page-main/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+
+### What I learned
+
+
+This function displays the pledge page and activate the active state of the pledge box
+
+```js
+const selectRewardBtn = document.querySelectorAll(".select");
+selectRewardBtn.forEach((element) => {
+  // console.log(reward)
+  element.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("p");
+    let getRadioButton, radioIndex;
+    if (element) {
+      radioIndex = element.id;
+      project();
+      getRadioButton = radioBtn[radioIndex];
+
+      getRadioButton.checked = true;
+      getRadioButton.scrollIntoView();
+      getRadioButton.parentElement.parentElement.parentElement.classList.add("box-bordercolor");
+      getRadioButton.parentElement.parentElement.nextElementSibling.classList.add("block");
+    } else {
+      getRadioButton.parentElement.parentElement.parentElement.classList.remove("box-bordercolor");
+      getRadioButton.parentElement.parentElement.nextElementSibling.classList.remove(
+        "block"
+      );
+    }
+  });
+});
+```
+
+
+**Note: Delete this note and the content within this section and replace with your own learnings.**
+
+### Continued development
+DOM Manipulation
+
+
+### Useful resources
+
+- [Example resource 1](https://www.example.com) - This helped me for XYZ reason. I really liked this pattern and will use it going forward.
+- [Example resource 2](https://www.example.com) - This is an amazing article which helped me finally understand XYZ. I'd recommend it to anyone still learning this concept.
+
+
+## Author
+
+
+- Frontend Mentor - [@ibimina](https://www.frontendmentor.io/profile/ibimina)
+- Twitter - [@ibiminaaH](https://www.twitter.com/ibiminaaH)
+
+**Note: Delete this note and add/remove/edit lines above based on what links you'd like to share.**
+
+## Acknowledgments
+
+This is where you can give a hat tip to anyone who helped you out on this project. Perhaps you worked in a team or got some inspiration from someone else's solution. This is the perfect place to give them some credit.
+
+**Note: Delete this note and edit this section's content as necessary. If you completed this challenge by yourself, feel free to delete this section entirely.**
---
+++ index.js
@@ -0,0 +1,202 @@
+
+
+
+
+const mobileToggle = document.querySelector(".mobile-navigation");
+const nav = document.querySelector(".navigation-wrap");
+
+
+mobileToggle.addEventListener("click", (e) => {
+  e.preventDefault();
+  const visibility = nav.getAttribute("data-visible");
+  if (visibility === "false") {
+    mobileToggle.setAttribute("aria-expanded", true);
+    nav.setAttribute("data-visible", true);
+  } else {
+    mobileToggle.setAttribute("aria-expanded", false);
+    nav.setAttribute("data-visible", false);
+  }
+});
+
+
+
+const backedProjectPage = document.querySelector(".selection-wrapper");
+const openBackProjectPage = document.querySelector(".donate");
+
+function project() {
+  const visibility = backedProjectPage.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    backedProjectPage.setAttribute("data-visible", true);
+  } else {
+    backedProjectPage.setAttribute("data-visible", false);
+  }
+}
+
+openBackProjectPage.addEventListener("click", project);
+
+
+const closeBackProjectPage = document.querySelector(".close");
+
+function closeProject() {
+  const visibility = backedProjectPage.getAttribute("data-visible");
+
+  if (visibility === "true") {
+    radioBtn.forEach((btn) => {
+      btn.checked = false;
+      if (!btn.checked) {
+        btn.parentElement.parentElement.parentElement.classList.remove("cy");
+        btn.parentElement.parentElement.nextElementSibling.classList.remove(
+          "bb"
+        );
+      }
+    });
+    backedProjectPage.setAttribute("data-visible", false);
+  }
+}
+
+closeBackProjectPage.addEventListener("click", closeProject);
+
+
+const radioBtn = document.querySelectorAll("input[type=radio]");
+
+function checkRadioBtnClick() {
+  radioBtn.forEach((btn) => {
+    if (btn.checked && btn.value === "25") {
+      btn.parentElement.parentElement.parentElement.classList.add("box-bordercolor");
+      btn.parentElement.parentElement.nextElementSibling.classList.add("block");
+    } else if (btn.checked && btn.value === "75") {
+      btn.parentElement.parentElement.parentElement.classList.add("box-bordercolor");
+      btn.parentElement.parentElement.nextElementSibling.classList.add("block");
+    } else if (btn.checked && btn.value === "0") {
+      backedProjectPage.removeAttribute("data-visible", false);
+      btn.checked = false;
+      successPage.classList.add("open");
+    } else {
+      btn.parentElement.parentElement.parentElement.classList.remove("box-bordercolor");
+      btn.parentElement.parentElement.nextElementSibling.classList.remove("block");
+    }
+  });
+}
+
+radioBtn.forEach((element) => {
+  element.addEventListener("change", checkRadioBtnClick);
+});
+
+const successPage = document.querySelector(".success-wrapper");
+
+const quantityLeft = document.querySelectorAll(".quan-id");
+const totalAmountPledge = document.querySelector(".total-amount");
+const totalPeopleBacked = document.querySelector(".total-back");
+const processPledge = document.querySelectorAll(".btn-wrap");
+
+processPledge.forEach((element) => {
+  
+  element.addEventListener("click", (e) => {
+    e.preventDefault();
+    let butn = e.target;
+    if (butn) {
+      let pledgeAmount = butn.previousElementSibling.value.trim();
+     
+      let pledgeAmountValid = pledgeAmount.length;
+
+      let enterValidPledgeDisplay = butn.nextElementSibling;
+     
+      let radioValue =
+        butn.parentElement.parentElement.parentElement.previousElementSibling
+          .children[0].children[0].value;
+    
+      let pledgePageQuantityLeft =
+        butn.parentElement.parentElement.parentElement.previousElementSibling.children[2]
+          .children[0];
+     
+      if (pledgeAmountValid && Number(pledgeAmount) >= Number(radioValue)) {
+        butn.previousElementSibling.value = "";
+       
+        radioBtn.forEach((btn) => {
+          btn.checked = false;
+          btn.parentElement.parentElement.parentElement.classList.remove("box-bordercolor");
+          btn.parentElement.parentElement.nextElementSibling.classList.remove(
+            "block"
+          );
+        });
+       
+        let back = Number(totalPeopleBacked.textContent.replaceAll(",", "")) + 1;
+
+        totalPeopleBacked.textContent = `${back.toLocaleString()}`;
+
+        let amount =
+          Number(
+            totalAmountPledge.textContent
+              .replaceAll("$", "")
+              .replaceAll(",", "")
+          ) + 1;
+        totalAmountPledge.textContent = `$${amount.toLocaleString()}`;
+
+        quantityLeft.forEach((element) => {
+          if (
+            element.dataset.action === radioValue &&
+            Number(element.textContent) >= 0
+          ) {
+            element.textContent = Number(element.textContent) - 1;
+            pledgePageQuantityLeft.textContent = Number(pledgePageQuantityLeft.textContent) - 1;
+          }
+        });
+
+        backedProjectPage.setAttribute("data-visible", false);
+        successPage.classList.add("open");
+      } else {
+        enterValidPledgeDisplay.textContent = `Enter $${radioValue} or above`;
+      }
+    }
+  });
+});
+
+const closeSuccessPage = document.querySelector(".got-it");
+
+closeSuccessPage.addEventListener("click", () => {
+  successPage.classList.remove("open");
+});
+
+const selectRewardBtn = document.querySelectorAll(".select");
+selectRewardBtn.forEach((element) => {
+  // console.log(reward)
+  element.addEventListener("click", (e) => {
+    e.preventDefault();
+    //console.log("p");
+    let getRadioButton, radioIndex;
+    if (element) {
+      radioIndex = element.id;
+      project();
+      getRadioButton = radioBtn[radioIndex];
+
+      getRadioButton.checked = true;
+      getRadioButton.scrollIntoView();
+      getRadioButton.parentElement.parentElement.parentElement.classList.add("box-bordercolor");
+      getRadioButton.parentElement.parentElement.nextElementSibling.classList.add("block");
+    } else {
+      getRadioButton.parentElement.parentElement.parentElement.classList.remove("box-bordercolor");
+      getRadioButton.parentElement.parentElement.nextElementSibling.classList.remove(
+        "block"
+      );
+    }
+  });
+});
+
+const bookmark = document.querySelector(".book-box");
+
+bookmark.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.matches("img") || e.target.matches("p")) {
+    const bookmarkText = document.querySelector(".book");
+    const bookmarkIcon = document.querySelector(".bookmark");
+
+    bookmarkText.classList.add("change-color");
+    bookmarkIcon.classList.add("active");
+
+    setTimeout(() => {
+      bookmarkText.classList.remove("change-color");
+      bookmarkIcon.classList.remove("active");
+    }, 1000);
+  }
+});
